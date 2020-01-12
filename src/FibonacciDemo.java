4850456879ac5e92b39805a0834bd714cdb8779c
nayemjoy <njnayem@gmail.com>
2020-01-12 17:49:43 -0500

    Thitd commit

---
+++ src/FibonacciDemo.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class FibonacciDemo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner ( System.in );
+        System.out.print ("Haw many number : ");
+        int n = input.nextInt ();
+
+        int first =0;
+        int second = 1;
+        int fibo;
+        System.out.print(first+" "+second);
+        for (int i = 3; i<=n; i++){
+            fibo= first + second;
+            System.out.println (" "+fibo);
+            first= second;
+            second=fibo;
+            System.out.println ();
+        }
+
+    }
+
+}
